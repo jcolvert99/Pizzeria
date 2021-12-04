ca28b04762ae4849ef81c0f0d6b7cf58055001ce
jcolvert99 <john_colvert2@baylor.edu>
2021-12-03 19:10:39 -0600

    Defined "Pizza" and "Topping" models

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+    def __str__(self):
+        return self.namess
