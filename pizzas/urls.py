135ad0f4809fc723157899831ef3361652612ba4
jcolvert99 <john_colvert2@baylor.edu>
2021-12-06 15:06:19 -0600

    Create pizza page with listed toppings

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -7,4 +7,5 @@ app_name = 'pizzas'
 urlpatterns = [
     path('',views.index, name='index'),
     path('pizzas',views.pizzas, name='pizzas'),
+    path('pizzas/<int:pizza_id>/',views.pizza,name='pizza')
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -9,9 +9,21 @@ def index(request):
     return render(request, 'pizzas/index.html')
 
 
+
 def pizzas(request):
     pizzas = Pizza.objects.order_by('name')
 
     context = {'pizzas':pizzas}
 
     return render(request, 'pizzas/pizzas.html',context)
+
+
+
+def pizza(request,pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+    toppings = pizza.topping_set.all()         #foregin key accessed using '_set'
+    context = {'pizza':pizza, 'toppings':toppings}
+
+    return render(request, 'pizzas/pizza.html',context)
+
