b5d089c725736d63ee3e3caf0b1a19bc2e71c0b3
jcolvert99 <john_colvert2@baylor.edu>
2021-12-06 14:23:55 -0600

    Added page to show available pizzas

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -6,4 +6,5 @@ app_name = 'pizzas'
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('pizzas',views.pizzas, name='pizzas'),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from pizzas.models import Pizza
+
 # Create your views here.
 
 def index(request):
     '''Home page for pizzeria'''
     return render(request, 'pizzas/index.html')
+
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by('name')
+
+    context = {'pizzas':pizzas}
+
+    return render(request, 'pizzas/pizzas.html',context)
